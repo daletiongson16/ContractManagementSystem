821b76d620f7e26b2306a9c86000d90f77ade2ac
daletiongson16 <66628236+daletiongson16@users.noreply.github.com>
2020-07-15 07:24:00 -0700

    Add files via upload

---
+++ ContractManagerClasses/Class1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract_Management
+{
+    public class Contract
+    {
+        public string company_name;
+        public int contract_id;
+        public bool active;
+        public string contract_name;
+
+        public Contract(string company_name, int contract_id, bool active, string contract_name)
+        {
+            this.company_name = company_name;
+            this.contract_id = contract_id;
+            this.active = active;
+            this.contract_name = contract_name;
+        }
+    }
+
+    public class ContractVersion
+    {
+        public int contract_id;
+        public DateTime date_revised;
+        public string path_to_file;
+        public int version_id;
+
+        public ContractVersion(int contract_id, DateTime date_revised, string path_to_file, int version_id)
+        {
+            this.contract_id = contract_id;
+            this.date_revised = date_revised;
+            this.path_to_file = path_to_file;
+            this.version_id = version_id;
+        }
+    }
+
+    public class Tag
+    {
+        public int tag_id;
+        public int version_id;
+        public string tag_name;
+        public int x_coord;
+        public int y_coord;
+
+        public Tag(int tag_id, int version_id, string tag_name, int x_coord, int y_coord)
+        {
+            this.tag_id = tag_id;
+            this.version_id = version_id;
+            this.tag_name = tag_name;
+            this.x_coord = x_coord;
+            this.y_coord = y_coord;
+        }
+    }
+
+    public class TagType
+    {
+        public string name;
+        public string value_type;
+        public string description;
+        public TagType(string name, string value_type, string description)
+        {
+            this.name = name;
+            this.value_type = value_type;
+            this.description = description;
+        }
+    }
+
+    public class ListEntry
+    {
+        public string counterparty;
+        public string contract;
+        public string version;
+        public int version_id;
+
+        public ListEntry(string counterparty, string contract, string version, int version_id)
+        {
+            this.counterparty = counterparty;
+            this.contract = contract;
+            this.version = version;
+            this.version_id = version_id;
+        }
+    }
+}
---
+++ StorageProvider/Class1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IStorageProvider
+{
+    public interface IStorageProvider
+    {
+        Task<bool> ExistsAsync(string filename);
+        Task<byte[]> ReadAsync(string filename);
+        Task WriteAsync(string filename, byte[] value);
+        Task CopyAsync(string filenameFrom, string filenameTo);
+        Task MoveAsync(string filenameFrom, string filenameTo);
+        Task DeleteAsync(string filename);
+        //Task<IList<string>> ListAsync(string path = null);
+    }
+    public class FileStore : IStorageProvider
+    {
+        public Task<bool> ExistsAsync(string filename)
+        {
+            return Task.Run(() =>
+            {
+                return File.Exists(filename);
+            });
+        }
+
+        public Task<byte[]> ReadAsync(string filename)
+        {
+            return Task.Run(() =>
+            {
+                byte[] bytes;
+                try
+                {
+                    using (FileStream fsSource = new FileStream(filename, FileMode.Open, FileAccess.Read))
+                    {
+                        bytes = new byte[fsSource.Length];
+                        int numBytesToRead = (int)fsSource.Length;
+                        int numBytesRead = 0;
+
+                        while (numBytesToRead > 0)
+                        {
+                            int n = fsSource.Read(bytes, numBytesRead, numBytesToRead);
+
+                            if (n == 0)
+                                break;
+
+                            numBytesRead += n;
+                            numBytesToRead -= n;
+                        }
+                    }
+                    return bytes;
+                }
+                catch (FileNotFoundException ioEx)
+                {
+                    Console.WriteLine(ioEx.Message);
+                    bytes = null;
+                    return bytes;
+                }
+            }
+            );
+        }
+
+        public Task WriteAsync(string filename, byte[] value)
+        {
+            return Task.Run(() =>
+            {
+                int numBytesToRead = (int)value.Length;
+
+                using (FileStream fsNew = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    fsNew.Write(value, 0, numBytesToRead);
+                }
+            });
+        }
+
+        public Task CopyAsync(string filenameFrom, string filenameTo)
+        {
+            return Task.Run(() =>
+            {
+                byte[] bytes;
+                try
+                {
+                    using (FileStream fsSource = new FileStream(filenameFrom, FileMode.Open, FileAccess.Read))
+                    {
+                        bytes = new byte[fsSource.Length];
+                        int numBytesToRead = (int)fsSource.Length;
+                        int numBytesRead = 0;
+
+                        while (numBytesToRead > 0)
+                        {
+                            int n = fsSource.Read(bytes, numBytesRead, numBytesToRead);
+
+                            if (n == 0)
+                                break;
+
+                            numBytesRead += n;
+                            numBytesToRead -= n;
+                        }
+                        numBytesToRead = bytes.Length;
+                        using (FileStream fsNew = new FileStream(filenameTo, FileMode.OpenOrCreate, FileAccess.Write))
+                        {
+                            fsNew.Write(bytes, 0, numBytesToRead);
+                        }
+                    }
+                }
+                catch (FileNotFoundException ioEx)
+                {
+                    Console.WriteLine(ioEx.Message);
+                }
+            });
+        }
+
+        public Task MoveAsync(string filenameFrom, string filenameTo)
+        {
+            return Task.Run(() =>
+            {
+                byte[] bytes;
+                try
+                {
+                    using (FileStream fsSource = new FileStream(filenameFrom, FileMode.Open, FileAccess.Read))
+                    {
+                        bytes = new byte[fsSource.Length];
+                        int numBytesToRead = (int)fsSource.Length;
+                        int numBytesRead = 0;
+
+                        while (numBytesToRead > 0)
+                        {
+                            int n = fsSource.Read(bytes, numBytesRead, numBytesToRead);
+
+                            if (n == 0)
+                                break;
+
+                            numBytesRead += n;
+                            numBytesToRead -= n;
+                        }
+                        File.Delete(filenameFrom);
+                        numBytesToRead = bytes.Length;
+                        using (FileStream fsNew = new FileStream(filenameTo, FileMode.OpenOrCreate, FileAccess.Write))
+                        {
+                            fsNew.Write(bytes, 0, numBytesToRead);
+                        }
+                    }
+                }
+                catch (FileNotFoundException ioEx)
+                {
+                    Console.WriteLine(ioEx.Message);
+                }
+            });
+        }
+
+        public Task DeleteAsync(string filename)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    File.Delete(filename);
+                }
+                catch (FileNotFoundException ioEx)
+                {
+                    Console.WriteLine(ioEx);
+                }
+            }
+            );
+        }
+
+    }
+}
